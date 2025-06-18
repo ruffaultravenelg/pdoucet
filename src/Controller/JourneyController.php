f0888a74b46c8c608bddae1e7140c4ddfa354275
ruffaultravenelg <gemino.ruffaultravenel@gmail.com>
2025-06-18 14:02:02 +0200

    Added journey create page

--- src/Controller/JourneyController.php
+++ src/Controller/JourneyController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Journey;
+use App\Form\JourneyType;
 use App\Repository\JourneyRepository;
-use Doctrine\ORM\EntityManager;
+use App\Service\AdminService;
+use App\Service\FileHandler;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,6 +24,35 @@ final class JourneyController extends AbstractController
         return $this->render('journey/index.html.twig', [
             'journeys' => $journeys,
         ]);
+    }
+
+    #[Route('/journey/new', name: 'journey_new')]
+    public function create(EntityManagerInterface $em, AdminService $adminService, Request $request, FileHandler $fileHandler): Response
+    {
+        // Check permission
+        if (!$adminService->isAdmin()) {
+            return $this->redirectToRoute('login');
+        }
+
+        $journey = new Journey();
+        $form = $this->createForm(JourneyType::class, $journey, ['submit_label' => 'Ajouter']);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($journey);
+            $em->flush();
+            
+            return $this->redirectToRoute('journey');
+
+        }
+        
+        // Render page
+        return $this->render('journey/new.html.twig', [
+            'form' => $form
+        ]);
+        
     }
 
 }
---
+++ src/Form/JourneyType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Journey;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JourneyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de l\'étape',
+                'attr' => [
+                    'placeholder' => 'Date de l\'étape',
+                    'class' => 'field',
+                ],
+            ])
+            ->add('location', TextType::class, [
+                'label' => 'Lieu',
+                'attr' => [
+                    'placeholder' => 'Lieu de l\'étape',
+                    'maxlength' => 255,
+                    'class' => 'field',
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Description de l\'étape',
+                    'class' => 'field',
+                    'multiline' => true,
+                ],
+                'required' => false,
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Adresse de l\'étape',
+                    'maxlength' => 255,
+                    'class' => 'field',
+                ],
+                'required' => false,
+            ])
+            ->add('lo', NumberType::class, [
+                'label' => 'Longitude',
+                'attr' => [
+                    'placeholder' => 'Longitude',
+                    'class' => 'field',
+                ],
+                'required' => false,
+            ])
+            ->add('la', NumberType::class, [
+                'label' => 'Latitude',
+                'attr' => [
+                    'placeholder' => 'Latitude',
+                    'class' => 'field',
+                ],
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => $options['submit_label'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Journey::class,
+            'submit_label' => 'Valider',
+        ]);
+    }
+}
