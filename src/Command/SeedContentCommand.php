8480bc7d250847db2f3bede509f598d69da41317
ruffaultravenelg <gemino.ruffaultravenel@gmail.com>
2025-07-01 09:14:12 +0200

    Updated content field type

--- src/Command/SeedContentCommand.php
+++ src/Command/SeedContentCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Service\ContentLoader;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,9 +19,20 @@ class SeedContentCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                'erase',
+                InputArgument::OPTIONAL,
+                'Erase existing settings before inserting new ones (true/false)',
+                'false'
+            );
+    }
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->loader->updateDB(false);
+        $erase = filter_var($input->getArgument('erase'), FILTER_VALIDATE_BOOLEAN);
+        $this->loader->updateDB($erase);
         $output->writeln('<info>Contenu inséré depuis le fichier YAML.</info>');
         return Command::SUCCESS;
     }
